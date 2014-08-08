4c7ef25af97f093c0ea8ae83d89915d167ee4fa3
caman9119 <ca.jones9119@gmail.com>
2014-08-07 20:13:42 -0400

    Update RssFeedReaderActivity.java

--- src/main/java/com/cjones/taskforcemamba/activity/RssFeedReaderActivity.java
+++ src/main/java/com/cjones/taskforcemamba/activity/RssFeedReaderActivity.java
@@ -1,191 +1,281 @@
 package com.cjones.taskforcemamba.activity;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
-import org.json.JSONObject;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.Log;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.cjones.taskforcemamba.About;
-import com.cjones.taskforcemamba.MainActivity;
 import com.cjones.taskforcemamba.R;
-import com.cjones.taskforcemamba.adapter.RssReaderListAdapter;
-import com.cjones.taskforcemamba.helper.ReverseOrder;
-import com.cjones.taskforcemamba.helper.RssFeedStructure;
-import com.cjones.taskforcemamba.helper.SortingOrder;
-import com.cjones.taskforcemamba.helper.XmlHandler;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
-public class RssFeedReaderActivity extends Activity {
-    /** Called when the activity is first created. */
-	
-	private final String feed = "http://feeds.nytimes.com/nyt/rss/HomePage";
-	//preppulating feed list with the same feeds since I don't have any others to use right now.
-	private final List<String> feeds= new ArrayList<String>(){{
-			add(feed); 
-			add(feed); 
-			add(feed);
-		}};
-	
-	List<ListView> _rssFeedListViews = new ArrayList<ListView>();
-	List<JSONObject> jobs ;
-	List<List<RssFeedStructure>> rssStrs;
-	String sorti = "";
-	String mode = "";
-	Button sort_Btn;
-    public static final String PREFS_NAME = "MyPrefsFile";
+/**
+ * Fragment used for managing interactions for and presentation of a navigation drawer.
+ * See the <a href="https://developer.android.com/design/patterns/navigation-drawer.html#Interaction">
+ * design guidelines</a> for a complete explanation of the behaviors implemented here.
+ */
+public class NavigationDrawerFragment extends Fragment {
 
+    /**
+     * Remember the position of the selected item.
+     */
+    private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
+
+    /**
+     * Per the design guidelines, you should show the drawer on launch until the user manually
+     * expands it. This shared preference tracks this.
+     */
+    private static final String PREF_USER_LEARNED_DRAWER = "navigation_drawer_learned";
+
+    /**
+     * A pointer to the current callbacks instance (the Activity).
+     */
+    private NavigationDrawerCallbacks mCallbacks;
+
+    /**
+     * Helper component that ties the action bar to the navigation drawer.
+     */
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerListView;
+    private View mFragmentContainerView;
+
+    private int mCurrentSelectedPosition = 0;
+    private boolean mFromSavedInstanceState;
+    private boolean mUserLearnedDrawer;
+
+    public NavigationDrawerFragment() {
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
-        .build();
-        ImageLoader.getInstance().init(config);
-        setContentView(R.layout.rssfeedreaderactivity);
-        for(String s : feeds){
-        	ListView lv = (ListView)findViewById(R.id.rssfeed_listview);
-        	lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-	
-	            @Override
-	            public void onItemClick(AdapterView<?> parent, View view, int position,
-	                                    long id) {
-	                //Display the Title and Date to the user when they click on an episode
-	//                Object o = _rssFeedListView.getItemAtPosition(position);
-	//                RssFeedStructure obj_itemDetails = (RssFeedStructure)o;
-	//                String objURL = obj_itemDetails.getImgLink();
-	//                if (objURL != null){
-	////                    Toast.makeText(getApplicationContext(), "objURL = " + objURL, Toast.LENGTH_LONG).show();
-	//                }
-	//                Intent showContent = new Intent(getApplicationContext(),
-	//                MainActivity.class);
-	//                showContent.setData(Uri.parse(objURL));
-	//                startActivity(showContent);
-	                Toast.makeText(getApplicationContext(), "Clicking on an item is a future enhancement, for now click the cloud icon to launch to the webview.", Toast.LENGTH_LONG).show();
-	            }
-	
-			});
-        	
-        	//add to our list of views
-        	_rssFeedListViews.add(lv);
+
+        // Read in the flag indicating whether or not the user has demonstrated awareness of the
+        // drawer. See PREF_USER_LEARNED_DRAWER for details.
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mUserLearnedDrawer = sp.getBoolean(PREF_USER_LEARNED_DRAWER, false);
+
+        if (savedInstanceState != null) {
+            mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
+            mFromSavedInstanceState = true;
         }
 
-       RssFeedTask rssTask = new RssFeedTask();
-       rssTask.execute();
-}
-    private class RssFeedTask extends AsyncTask<String, Void, String> {
-		// private String Content;
-		private ProgressDialog Dialog;
-		String response = "";
-
-		@Override
-		protected void onPreExecute() {
-			Dialog = new ProgressDialog(RssFeedReaderActivity.this);
-			Dialog.setMessage("Rss Loading...");
-			Dialog.show();
-		
-		}
-
-		@Override
-		protected String doInBackground(String... urls) {
-			  try {
-				  
-				  
-				  for(String s : feeds) { 
-				  	XmlHandler rh = new XmlHandler();
-				  	rssStrs.add(rh.getLatestArticles(s));
-				  }
-			  } catch (Exception e) {}
-			return response;
-
-		}
-
-		@Override
-		protected void onPostExecute(String result) {
-			  if(sorti.equalsIgnoreCase("sort")){
-				  sorti = "reverse";
-				 for(List<RssFeedStructure> struct : rssStrs){
-					 Collections.sort(struct, new SortingOrder());
-					 }
-			     
-			     
-			  }else if(sorti.equalsIgnoreCase("reverse")){
-				  sorti = "normal";
-				  Comparator comp = Collections.reverseOrder();
-				  for(List<RssFeedStructure> struct : rssStrs){
-						 Collections.sort(struct, new ReverseOrder());
-				  }
-			  }else{
-				  sorti = "";
-			  }
-			  if(rssStrs != null){
-			  	int adapterCt = 0;
-			  	if(_rssFeedListViews == null) return;
-			  	for(ListView lv : _rssFeedListViews){
-			  		if(adapterCt < rssStrs.size()){
-			  			lv.setAdapter(new RssReaderListAdapter(RssFeedReaderActivity.this, rssStrs.get(adapterCt)));
-			  		}
-			  	}
-			  }
-			  Dialog.dismiss();
-		}
-	}
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_list, menu);
-        return super.onCreateOptionsMenu(
-                menu);
+        // Select either the default item (0) or the last selected item.
+        selectItem(mCurrentSelectedPosition);
     }
+
+    @Override
+    public void onActivityCreated (Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // Indicate that this fragment would like to influence the set of actions in the action bar.
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        mDrawerListView = (ListView) inflater.inflate(
+                R.layout.fragment_navigation_drawer, container, false);
+        mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItem(position);
+            }
+        });
+        mDrawerListView.setAdapter(new ArrayAdapter<String>(
+                getActionBar().getThemedContext(),
+                android.R.layout.simple_list_item_activated_1,
+                android.R.id.text1,
+                new String[]{
+                        getString(R.string.title_section1),
+                        getString(R.string.title_section2),
+                        "Blog",
+                        getString(R.string.title_section3),
+
+                }));
+        mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
+        return mDrawerListView;
+    }
+
+    public boolean isDrawerOpen() {
+        return mDrawerLayout != null && mDrawerLayout.isDrawerOpen(mFragmentContainerView);
+    }
+
+    /**
+     * Users of this fragment must call this method to set up the navigation drawer interactions.
+     *
+     * @param fragmentId   The android:id of this fragment in its activity's layout.
+     * @param drawerLayout The DrawerLayout containing this fragment's UI.
+     */
+    public void setUp(int fragmentId, DrawerLayout drawerLayout) {
+        mFragmentContainerView = getActivity().findViewById(fragmentId);
+        mDrawerLayout = drawerLayout;
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // set up the drawer's list view with items and click listener
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the navigation drawer and the action bar app icon.
+        mDrawerToggle = new ActionBarDrawerToggle(
+                getActivity(),                    /* host Activity */
+                mDrawerLayout,                    /* DrawerLayout object */
+                R.drawable.ic_drawer,             /* nav drawer image to replace 'Up' caret */
+                R.string.navigation_drawer_open,  /* "open drawer" description for accessibility */
+                R.string.navigation_drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                if (!isAdded()) {
+                    return;
+                }
+
+                getActivity().invalidateOptionsMenu(); // calls onPrepareOptionsMenu()
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                if (!isAdded()) {
+                    return;
+                }
+
+                if (!mUserLearnedDrawer) {
+                    // The user manually opened the drawer; store this flag to prevent auto-showing
+                    // the navigation drawer automatically in the future.
+                    mUserLearnedDrawer = true;
+                    SharedPreferences sp = PreferenceManager
+                            .getDefaultSharedPreferences(getActivity());
+                    sp.edit().putBoolean(PREF_USER_LEARNED_DRAWER, true).apply();
+                }
+
+                getActivity().invalidateOptionsMenu(); // calls onPrepareOptionsMenu()
+            }
+        };
+
+        // If the user hasn't 'learned' about the drawer, open it to introduce them to the drawer,
+        // per the navigation drawer design guidelines.
+        if (!mUserLearnedDrawer && !mFromSavedInstanceState) {
+            mDrawerLayout.openDrawer(mFragmentContainerView);
+        }
+
+        // Defer code dependent on restoration of previous instance state.
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();
+            }
+        });
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    private void selectItem(int position) {
+        mCurrentSelectedPosition = position;
+        if (mDrawerListView != null) {
+            mDrawerListView.setItemChecked(position, true);
+        }
+        if (mDrawerLayout != null) {
+            mDrawerLayout.closeDrawer(mFragmentContainerView);
+        }
+        if (mCallbacks != null) {
+            mCallbacks.onNavigationDrawerItemSelected(position);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mCallbacks = (NavigationDrawerCallbacks) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException("Activity must implement NavigationDrawerCallbacks.");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mCallbacks = null;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(STATE_SELECTED_POSITION, mCurrentSelectedPosition);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Forward the new configuration the drawer toggle component.
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // If the drawer is open, show the global app actions in the action bar. See also
+        // showGlobalContextActionBar, which controls the top-left area of the action bar.
+        if (mDrawerLayout != null && isDrawerOpen()) {
+            inflater.inflate(R.menu.global, menu);
+            showGlobalContextActionBar();
+        }
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.apply:
-                Log.i(TAG, "Login trigger");
-                Intent showContent = new Intent(this, MainActivity.class);
-                showContent.setData(Uri.parse("http://www.tfmamba.com/register.php"));
-                startActivity(showContent);
-//                myWebView.loadUrl("http://www.tfmamba.com/register.php");
-                return true;
-            case R.id.Web:
-                Log.i(TAG, "Web trigger");
-                Intent homeContent = new Intent(this, MainActivity.class);
-                homeContent.setData(Uri.parse("http://www.tfmamba.com"));
-                startActivity(homeContent);
-//                myWebView.loadUrl("http://www.tfmamba.com");
-                return true;
-            case R.id.youtube:
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=ILAT3qPB1FA")));
-                return true;
-            case R.id.about:
-                Log.i(TAG, "About trigger");
-                Intent AboutIntent = new Intent(this, About.class);
-                startActivity(AboutIntent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
-    }
-    public static final String TAG = "Task Force Mamba";
 
-  
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Per the navigation drawer design guidelines, updates the action bar to show the global app
+     * 'context', rather than just what's in the current screen.
+     */
+    private void showGlobalContextActionBar() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setTitle(R.string.app_name);
+    }
+
+    private ActionBar getActionBar() {
+        return getActivity().getActionBar();
+    }
+
+    /**
+     * Callbacks interface that all activities using this fragment must implement.
+     */
+    public static interface NavigationDrawerCallbacks {
+        /**
+         * Called when an item in the navigation drawer is selected.
+         */
+        void onNavigationDrawerItemSelected(int position);
+    }
 }
